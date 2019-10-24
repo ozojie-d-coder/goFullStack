799e1dab692019ab38e8f8e6ea4e3df0fa578c47
ozojie-d-coder <ozojie3@yahoo.com>
2019-10-24 13:31:21 +0100

    Go Fullstack with NodeJS, Express and MongoDB

---
+++ app.js
@@ -0,0 +1,164 @@
+//MbyA5w9I0fewCaMj
+//mongodb+srv://josephojielu:<pass123456>@ozojie-aevnu.mongodb.net/test?retryWrites=true&w=majority
+
+
+/*
+issues:
+recipe/5daba13b9244e518d099548a:1 Access to XMLHttpRequest at 
+'http://localhost:3000/api/recipes/5daba13b9244e518d099548a' from origin 'http://localhost:4200' 
+has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the 
+requested resource
+Solution:
+npm install --save cors
+var cors = require('cors');
+app.use(cors());
+*/
+
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Recipe = require('./models/recipe');
+var cors = require('cors');
+
+
+mongoose.connect('mongodb+srv://josephojielu:MbyA5w9I0fewCaMj@ozojie-aevnu.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+
+  //dependencies
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+//post request
+app.post('/api/recipes', (req, res, next) => {
+  const recipe = new Recipe({
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time,
+    recipeId: req.body.recipe
+  });
+  recipe.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//cross origin
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+
+//single page
+app.get('/api/recipes/:id', (req, res, next) => {
+  Recipe.findOne({
+    _id: req.params.id
+  }).then(
+    (recipe) => {
+      res.status(200).json(recipe);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+
+
+//update route
+app.put('/api/recipes/:id', (req, res, next) => {
+  const recipe = new Recipe({
+    _id: req.params.id,
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time,
+    recipeId: req.body.recipe
+  });
+  Recipe.updateOne({_id: req.params.id}, recipe).then(
+    () => {
+      res.status(201).json({
+        message: 'Recipe updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//delete route
+app.delete('/api/recipes/:id', (req, res, next) => {
+  Recipe.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//get request
+  app.use('/api/recipes', (req, res, next) => {
+    Recipe.find().then(
+      (recipes) => {
+        res.status(200).json(recipes);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+
+  module.exports = app;
+
+
+
+
+
+
+
+
+  
